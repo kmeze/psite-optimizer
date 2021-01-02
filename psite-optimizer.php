49451d0ef981bae622172109515a0a6d134346c4
Kresimir Meze <kresimir.meze@gmail.com>
2021-01-02 08:51:12 +0100

    Added main plugin file with required header

---
+++ psite-optimizer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Psite Optimizer
+ * Plugin URI:        https://github.com/kmeze/psite-optimizer
+ * Description:       Turns off unnecessary WordPress features and speeds up page loading.
+ * Version:           1.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * Author:            Krešimir Meze
+ * Author URI:        https://github.com/kmeze
+ * License:           GPL v3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       psopt
+ * Domain Path:       /languages
+ */
+ 
